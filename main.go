9e362f22dd18a1f555e66ad7d9fd1bcbe5b832ee
Anuvind-ap <anuvindap2016@gmail.com>
2024-10-03 16:08:02 +0530

    content extracted

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+)
+
+func main() {
+	dir, err := os.Open("statements")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dir.Close()
+
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, file := range files {
+		filename := file.Name()
+		pdfPath := "statements/" + filename
+		err := api.ExtractContentFile(pdfPath, "output/extracted_text.txt", nil, nil)
+		if err != nil {
+			log.Fatalf("Error extracting text from PDF: %s\n", err)
+		}
+
+		fmt.Println("Text extraction complete! Check output/extracted_text.txt for results.")
+	}
 }
